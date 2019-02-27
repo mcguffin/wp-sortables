ad7b23e0c4c40a427969c45e08e5d318853fc023
mcguffin <joern@flyingletters.com>
2019-02-27 17:54:55 +0100

    Release version 0.0.12

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ Plugin Name: WP Sortables
 Plugin URI: http://wordpress.org/
 Description: Enter description here.
 Author: Jörn Lund
-Version: 0.0.11
+Version: 0.0.12
 Author URI: https://github.com/mcguffin
 License: GPL3
 Github Repository: mcguffin/wp-sortables
